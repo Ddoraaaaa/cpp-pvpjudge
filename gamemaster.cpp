c23b07e1ea99da44123111cb274f655639b7897d
Ddoraaaaa <ilbp7024@gmail.com>
2023-04-14 23:56:38 +1000

    duwa

--- gamemaster.cpp
+++ gamemaster.cpp
@@ -1,79 +1,143 @@
-#include <bits/stdc++.h>
-#include <sstream>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdio>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+// not available on OS X, because why would it
+#ifndef __APPLE__
+#include <seccomp.h>
+#endif
+
 using namespace std;
 
-int curPlayer;
-int a[6][8], cntMove=0;
+class exFile {
+    enum Languages {
+        py, cpp, c, java, go, ruby, js
+    };
 
-bool checkPos(int x, int y) {
-    if(x < 0 || x > 5 || y < 1 || y > 7 || a[x][y]!=curPlayer) {
-        return false;
-    } 
-    return true;
-}
+    int toFile[2], fromFile[2];
+    FILE *toF, *frF;
+    bool _restricted;
 
-bool checkRow(int x, int y) {
-    bool check0=true, check1=true, check2=true;
-    for(int i=0; i<=4; i++) {
-        check0&=checkPos(x, y+i);
-        check1&=checkPos(x+i, y);
-        check2&=checkPos(x+i, y+i);
-    }
-    return check0|check1|check2;
-}
+public:
+    exFile(bool restricted = true) : _restricted(restricted) {;}
 
-string check(int pos) {
-    if(pos < 1 || pos > 7) {
-        return "invalid";
-    }
-    int row = 6;
-    while(row >=1 && a[row-1][pos] == 0) row--;
-    if(row==6) return "invalid";
-    a[row][pos]=curPlayer;
-    cntMove++;
-    for(int i=0; i<6; i++) {
-        for(int j=1; j<=7; j++) {
-            if(a[i][j]=curPlayer) {
-                if(checkRow(i, j)) {
-                    return "win";
-                }
+    void runFile(string fileName, string fileRoot = "") {
+        
+        pid_t pid;
+
+        if (pipe(toFile) == -1) {
+            cerr << "Error: Failed to create pipe from main to file " << fileName << endl;
+            exit(1);
+        }
+        if (pipe(fromFile) == -1) {
+            cerr << "Error: Failed to create pipe from file " << fileName << " to main" <<endl;
+            exit(1);
+        }
+
+        pid = fork();
+        if (pid == -1) {
+            cerr << "Error: Failed to create child process for file " << fileName << endl;
+            exit(1);
+        } else if (pid == 0) {
+            // child process (File)
+
+            // pipe from main
+            {
+                // close write end of the pipe
+                close(toFile[1]);
+                // redirect stdin to read end of the pipe
+                dup2(toFile[0], STDIN_FILENO);
             }
-        }
-    }
-    if(cntMove != 42) {
-        return "valid";
-    }
-    else {
-        return "draw";
-    }
-}
 
-signed main() {
-    // print the original game state
-    cout<<"gamestate 0"<<endl;
+            // pipe to main
+            {
+                // close write end of the pipe
+                close(fromFile[0]);
+                // redirect stdout to the write end of the pipe
+                dup2(fromFile[1], STDOUT_FILENO);
+            }
 
-    // start game loop
-    string s;
-    stringstream strin;
-    while(true) {
-        // get player ID and number of lines
-        int lineCnt;
-        getline(cin, s); strin.str(s); strin >> curPlayer >> lineCnt;
+            if(_restricted) {
+                setRestriction(fileRoot);
+            }
+            
+            // execute file
+            execl(("./" + fileName).c_str(), fileName.c_str(), NULL);
 
-        int pos;
-        if(lineCnt != 1) {
-            return 0;
-        }
-        getline(cin, s); strin.str(s); strin >> pos;
-        string turnRes = check(pos);
-        if(turnRes == "win") {
-            cout<<"WIN"<<endl;
-        } else if(turnRes == "draw") {
-            cout<<"DRAW"<<endl;
-        } else if(turnRes == "valid") {
-            cout<<"VALID 1"<<endl<<pos<<endl;
+            // The code below is only executed if execl() fails
+            cerr << "Error: Failed to execute player1" << endl;
+
+            exit(1);
         } else {
-            cout<<"LOSE"<<endl;
+            // parent process (main)
+
+            // close unneeded pipes:
+            close(toFile[0]);
+            close(fromFile[1]);
+            
+            // open file streams
+            toF = fdopen(toFile[1], "w");
+            frF = fdopen(fromFile[0], "r");
+            
+            // cout << "Response from player1: " << response << endl;
+
+            // Wait for player1 to finish
+            wait(NULL);
+                
         }
+    }
+
+    string readLine(string &s) {
+        s = "";
+        char buf[1024];
+        while(fgets(buf, sizeof(buf), frF) != nullptr) {
+            s += buf;
+        };
+    }
+
+    void writeLine(string &s) {
+        fprintf(toF, s.c_str());
+    }
+
+private:
+    void setRestriction(string fileRoot) {
+        struct rlimit limits;
+
+        // limit cpu time (1s)
+        limits.rlim_cur = 1;
+        limits.rlim_max = 1;
+        setrlimit(RLIMIT_CPU, &limits);
+
+        // limit mem usage (1mb)
+        limits.rlim_cur = 1024 * 1024;
+        limits.rlim_max = 1024 * 1024;
+        setrlimit(RLIMIT_AS, &limits);
+
+        // sandbox process file access
+        chdir("/");
+        chroot(fileRoot.c_str());
+
+        //kill syscalls
+        #ifndef __APPLE__
+        scmp_filter_ctx ctx;
+        ctx = seccomp_init(SCMP_ACT_ALLOW);
+        seccomp_rule_add(ctx, SCMP_ACT_KILL, SCMP_SYS(execve), 0);
+        seccomp_rule_add(ctx, SCMP_ACT_KILL, SCMP_SYS(socket), 0);
+        seccomp_load(ctx);
+        #endif
+    }
+};
+
+int main() {
+    exFile player1(false), player2(true), judge(false);
+    string p1F = "player1", p2F = "player2", judgeF = "judge";
+    judge.runFile(judgeF);
+    player1.runFile(p1F);
+    player2.runFile(p2F);
+    while(true) {
+
     }
 }
--- judge.cpp
+++ judge.cpp
@@ -1,103 +1,82 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <cstdio>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-// not available on OS X, because why would it
-// #include <seccomp.h>
-
+#include <bits/stdc++.h>
+#include <sstream>
 using namespace std;
 
-vector<int> exec_file(string filename, bool restricted) {
-    if(restricted) {
-        struct rlimit limits;
+int curPlayer;
+int a[6][8], cntMove=0;
 
-        // limit cpu time
-        limits.rlim_cur = 1;
-        limits.rlim_max = 1;
-        setrlimit(RLIMIT_CPU, &limits);
+bool checkPos(int x, int y) {
+    if(x < 0 || x > 5 || y < 1 || y > 7 || a[x][y]!=curPlayer) {
+        return false;
+    } 
+    return true;
+}
 
-        // limit mem usage
-        limits.rlim_cur = 1024 * 1024;
-        limits.rlim_max = 1024 * 1024;
-        setrlimit(RLIMIT_AS, &limits);
+bool checkRow(int x, int y) {
+    bool check0=true, check1=true, check2=true;
+    for(int i=0; i<=4; i++) {
+        check0&=checkPos(x, y+i);
+        check1&=checkPos(x+i, y);
+        check2&=checkPos(x+i, y+i);
+    }
+    return check0|check1|check2;
+}
 
-        // sandbox process file access
-        chdir("/");
-        chroot("/path/to/sandbox/directory");
-
-        //kill syscalls
-        // scmp_filter_ctx ctx;
-        // ctx = seccomp_init(SCMP_ACT_ALLOW);
-        // seccomp_rule_add(ctx, SCMP_ACT_KILL, SCMP_SYS(execve), 0);
-        // seccomp_rule_add(ctx, SCMP_ACT_KILL, SCMP_SYS(socket), 0);
-        // seccomp_load(ctx);
+string check(int pos) {
+    if(pos < 1 || pos > 7) {
+        return "invalid";
+    }
+    int row = 6;
+    while(row >=1 && a[row-1][pos] == 0) row--;
+    if(row==6) return "invalid";
+    a[row][pos]=curPlayer;
+    cntMove++;
+    for(int i=0; i<6; i++) {
+        for(int j=1; j<=7; j++) {
+            if(a[i][j]=curPlayer) {
+                if(checkRow(i, j)) {
+                    return "win";
+                }
+            }
+        }
+    }
+    if(cntMove != 42) {
+        return "valid";
+    }
+    else {
+        return "draw";
     }
 }
 
-int main() {
-    int judge_to_p1[2];
-    int p1_to_judge[2];
-    pid_t pid;
+signed main() {
+    // time limit for each player is 1000ms
+    cout << "time 1000" << endl;
 
-    if (pipe(judge_to_p1) == -1) {
-        cerr << "Error: Failed to create pipe from judge to p1" << endl;
-        return 1;
+    // print the original game state
+    cout << "gamestate 0" << endl;
+
+    // start game loop
+    string s;
+    stringstream strin;
+    while(true) {
+        // get player ID and number of lines
+        int lineCnt;
+        getline(cin, s); strin.str(s); strin >> curPlayer >> lineCnt;
+
+        int pos;
+        if(lineCnt != 1) {
+            return 0;
+        }
+        getline(cin, s); strin.str(s); strin >> pos;
+        string turnRes = check(pos);
+        if(turnRes == "win") {
+            cout<<"WIN"<<endl;
+        } else if(turnRes == "draw") {
+            cout<<"DRAW"<<endl;
+        } else if(turnRes == "valid") {
+            cout<<"VALID 1"<<endl<<pos<<endl;
+        } else {
+            cout<<"LOSE"<<endl;
+        }
     }
-    if (pipe(p1_to_judge) == -1) {
-        cerr << "Error: Failed to create pipe from p1 to judge" << endl;
-        return 1;
-    }
-
-    pid = fork();
-    if (pid == -1) {
-        cerr << "Error: Failed to create child process." << endl;
-        return 1;
-    } else if (pid == 0) {
-        // Child process (player1)
-
-        // pipe from judge
-        // Close the write end of the pipe
-        close(judge_to_p1[1]);
-        // Redirect stdin to the read end of the pipe
-        dup2(judge_to_p1[0], STDIN_FILENO);
-
-        // pipe to judge
-        // Close the write end of the pipe
-        close(p1_to_judge[0]);
-        // Redirect stdout to the write end of the pipe
-        dup2(p1_to_judge[1], STDOUT_FILENO);
-
-        // Execute player1
-        execl("./player1", "player1", NULL);
-
-        // The code below is only executed if execl() fails
-        cerr << "Error: Failed to execute player1" << endl;
-        return 1;
-    } else {
-        // Parent process (judge)
-
-        // Close unneeded pipes:
-        close(judge_to_p1[0]);
-        close(p1_to_judge[1]);
-        
-        FILE* to_p1 = fdopen(judge_to_p1[1], "w");
-        FILE* fr_p1 = fdopen(p1_to_judge[0], "r");
-
-        // Read data from player1 via the read end of the pipe
-        char ok[1024];
-        fgets(ok, sizeof(ok), fr_p1);
-        printf("%s", ok);
-        
-        // string response(buffer);
-
-        // cout << "Response from player1: " << response << endl;
-
-        // Wait for player1 to finish
-        wait(NULL);
-    }
-
-    return 0;
 }
