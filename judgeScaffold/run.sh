b37aa0463c079eef5aa2a55617eddced4af8deab
Ddoraaaaa <ilbp7024@gmail.com>
2023-04-15 15:12:35 +1000

    config stuff

--- judgeScaffold/run.sh
+++ judgeScaffold/run.sh
@@ -2,4 +2,4 @@ clang++ -Wall -std=c++17 ./player1.cpp -o player1
 clang++ -Wall -std=c++17 ./player2.cpp -o player2
 clang++ -Wall -std=c++17 ./gameMaster.cpp -o gameMaster
 clang++ -std=c++17 ./judge.cpp -o judge
-./judge
+./gameMaster
